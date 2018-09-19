aa498d726f905d0cd1dcc2a3f81b2a883765ed64
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-19 12:11:03 -0700

    Fixed subtraction function

--- arithmetic.py
+++ arithmetic.py
@@ -4,12 +4,13 @@
 def add(num1, num2):
     """Return the sum of the two inputs."""
     a = num1 + num2
-    return a
+    return  a
 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
-
+    difference = num1 - num2 
+    return difference
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
